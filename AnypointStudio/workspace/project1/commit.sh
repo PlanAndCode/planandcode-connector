6d1b94c36ccfbcd55c550f6167c51b588288ea64
fkiraz <f.kiraz@gtu.edu.tr>
2017-12-11 06:56:57 -0800

    project

---
+++ AnypointStudio/workspace/project1/commit.sh
@@ -0,0 +1,5 @@
+git add *
+git commit -m "project"
+git remote add origin https://github.com/PlanAndCode/planandcode-connector.git
+git config --global credential.helper 'cache --timeout 7200'
+git push -u origin master
