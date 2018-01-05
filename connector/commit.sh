09b22af331e5f6afc0b2e2cfe455bbd321a9a294
fkiraz <f.kiraz@gtu.edu.tr>
2018-01-05 10:33:58 -0800

    project

---
+++ connector/commit.sh
@@ -0,0 +1,5 @@
+git add -u
+git add *
+git commit -m "project"
+git remote add origin https://github.com/PlanAndCode/planandcode-connector.git
+git push -u origin master --force
---
+++ connector/pull.sh
@@ -0,0 +1,3 @@
+git remote add origin https://github.com/PlanAndCode/planandcode-connector.git
+git config --global credential.helper 'cache --timeout 7200'
+git pull origin master
